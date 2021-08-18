023ece7f6363cc3ba47cf83f5d55faa2797c82aa
ninaokumura <elaineokumura@gmail.com>
2021-08-18 12:51:04 +1200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2e2a2a8b-d80c-440b-9237-c8de51b35420';
+export const MODAL_CLOSE_SEC = 2.5;
